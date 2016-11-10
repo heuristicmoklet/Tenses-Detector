3384ca586ea21fb3a6cba4f9171d3b0f466d07ba
Ritza Kufita Rizqia <ritzakufita@gmail.com>
2016-11-10 20:00:11 +0700

    -Raw File Reader

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl104132231/tensesdetector/CSVFile.java
@@ -0,0 +1,46 @@
+package id.sch.smktelkom_mlg.project.xirpl104132231.tensesdetector;
+
+/**
+ * Created by LENOVO Z40-75 on 10/11/2016.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+package com.javapapers.android.csvfileread.app;
+
+public class CSVFile {
+    InputStream inputStream;
+
+    public CSVFile(InputStream inputStream){
+        this.inputStream = inputStream;
+    }
+
+    public List read(){
+        List resultList = new ArrayList();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        try {
+            String csvLine;
+            while ((csvLine = reader.readLine()) != null) {
+                String[] row = csvLine.split(",");
+                resultList.add(row);
+            }
+        }
+        catch (IOException ex) {
+            throw new RuntimeException("Error in reading CSV file: "+ex);
+        }
+        finally {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Error while closing input stream: "+e);
+            }
+        }
+        return resultList;
+    }
+}
